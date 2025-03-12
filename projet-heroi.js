817d0e1a12df4e4c99e61a7d100aa821a50ca62d
user <projetointegraai@gmail.com>
2025-03-12 17:02:47 -0300

    Primeiro commit do projeto herói

---
+++ projet-heroi.js
@@ -0,0 +1,35 @@
+function classificarHeroi(nome, xp) {
+    let nivel;
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
+
+const herois = [
+    { nome: "Aragorn", xp: 500 },
+    { nome: "Legolas", xp: 1500 },
+    { nome: "Gandalf", xp: 6000 },
+    { nome: "Frodo", xp: 8500 },
+    { nome: "Sauron", xp: 12000 }
+];
+
+for (let heroi of herois) {
+    classificarHeroi(heroi.nome, heroi.xp);
+}
